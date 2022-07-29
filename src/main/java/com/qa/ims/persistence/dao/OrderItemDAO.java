db655aa155cb736ad6bbd567f8b1d25a3fdc318b
dejanmccreery <dejanmccreery@gmail.com>
2022-07-29 02:41:45 +0100

    fixed clerical errors

--- src/main/java/com/qa/ims/persistence/dao/OrderItemDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/OrderItemDAO.java
@@ -2,7 +2,10 @@ package com.qa.ims.persistence.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import com.qa.ims.persistence.domain.Item;
 import com.qa.ims.persistence.domain.OrderItem;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -39,15 +42,17 @@ public class OrderItemDAO {
     /**
      * Deletes an item in the database
      *
-     * @param id - id of the item
+     * @param orderID - id of the order
+     * @param itemID - id of the item
      */
 
-    public int deleteItem(Long id) {
+    public int deleteItem(Long orderID, Long itemID) {
         try (Connection connection = DBUtils.getInstance().getConnection();
              PreparedStatement statement = connection.prepareStatement(
-                     "DELETE FROM order_item WHERE id = ?");
+                "DELETE FROM order_item WHERE order_id = ? AND item_id = ?");
         ) {
-            statement.setLong(1, id);
+            statement.setLong(1, orderID);
+            statement.setLong(2, itemID);
             return statement.executeUpdate();
         } catch (Exception e) {
             LOGGER.debug(e);
